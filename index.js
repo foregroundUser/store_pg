e88de3d40a1d20e679b87540932ca5ad83d56745
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-25 18:16:01 +0500

    products CRUD methods adding

--- index.js
+++ index.js
@@ -8,3 +8,45 @@ pool.connect().then(() => {
     console.log(err);
 });
 app.use(express.json());
+
+
+app.get('/products', async (req, res) => {
+    try {
+        const result = await pool.query('select * from products');
+        const isHave = result.rows.length > 0;
+        if (isHave) {
+            res.status(200).json({
+                success: true,
+                data: result.rows
+            });
+        } else {
+            res.status(200).json({
+                success: false,
+                message: 'no data found'
+            });
+        }
+
+    } catch (error) {
+        res.status(200).json({
+            success: false,
+            message: error.message
+        });
+    }
+})
+
+app.get('/products/:id', async (req, res) => {
+    const id = req.params;
+    const result = await poo.query('select * from products where id=$1', [id]);
+    const data = result.rows.length === 1;
+    if (data) {
+        res.status(200).json({
+            success: true,
+            data: result.rows
+        });
+    } else {
+        res.status(200).json({
+            success: false,
+            message: 'no data found'
+        });
+    }
+})
