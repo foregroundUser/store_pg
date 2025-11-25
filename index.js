0d69b9838b7abbdf29f42b8dca1449336bf07ce6
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-25 18:43:14 +0500

    Create delete update get done

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ pool.connect().then(() => {
 });
 app.use(express.json());
 
+app.post('/products', async (req, res) => {
+    try {
+        const {title, price, description, image, categoryId} = req.body
+        const result = await pool.query(`
+            insert into products(title, price, description, image, categoryId)
+            values ($1, $2, $3, $4, $5)
+            returning *
+        `, [title, price, description, image, categoryId])
+        res.status(200).json(result.rows);
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+        console.log(error)
+    }
+})
 
 app.get('/products', async (req, res) => {
     try {
@@ -34,9 +51,15 @@ app.get('/products', async (req, res) => {
     }
 })
 
+app.put('/products/:id', async (req, res) => {
+    const {id} = req.params;
+    const {title, image, description, price, categoryId} = req.body;
+    const response = await pool.query('update products  set title=$1, image=$2,description=$3,price=$4,categoryId=$5', [title, image, description, price, categoryId])
+});
+
 app.get('/products/:id', async (req, res) => {
-    const id = req.params;
-    const result = await poo.query('select * from products where id=$1', [id]);
+    const {id} = req.params;
+    const result = await pool.query('select * from products where id=$1', [id]);
     const data = result.rows.length === 1;
     if (data) {
         res.status(200).json({
@@ -51,6 +74,6 @@ app.get('/products/:id', async (req, res) => {
     }
 })
 
-app.listen(5000,"localhost", () => {
-    console.log('server is running');
+app.listen(5000, () => {
+    console.log('server is running ');
 })
