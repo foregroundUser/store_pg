1f1b9c1d94bcaacbeecdd72ab5c1954afd37fa5e
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-25 18:08:41 +0500

    pg connection added

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Happy developing ✨')
+const express = require('express');
+const app = express();
+const {Pool} = require('pg');
+const pool = new Pool({host: 'localhost', database: 'pgLearn', user: 'postgres', password: '2255', port: 5432});
+pool.connect().then(() => {
+    console.log('connected');
+}).catch((err) => {
+    console.log(err);
+});
+app.use(express.json());
