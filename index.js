01253e6003d22da125d35975ef4bde89c5a1b592
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-27 15:17:59 +0500

    making category routes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ pool.connect().then(() => {
 });
 app.use(express.json());
 
+//Products API
 app.post('/products', async (req, res) => {
     try {
         const {title, price, description, image, categoryId} = req.body
@@ -20,8 +21,7 @@ app.post('/products', async (req, res) => {
         res.status(200).json(result.rows);
     } catch (error) {
         res.status(500).json({
-            success: false,
-            message: error.message
+            success: false, message: error.message
         })
         console.log(error)
     }
@@ -33,28 +33,81 @@ app.get('/products', async (req, res) => {
         const isHave = result.rows.length > 0;
         if (isHave) {
             res.status(200).json({
-                success: true,
-                data: result.rows
+                success: true, data: result.rows
             });
         } else {
             res.status(200).json({
-                success: false,
-                message: 'no data found'
+                success: false, message: 'no data found'
             });
         }
 
     } catch (error) {
         res.status(200).json({
-            success: false,
-            message: error.message
+            success: false, message: error.message
+        });
+    }
+})
+
+app.delete('/products/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const result = await pool.query('delete from products where id=$1', [id]);
+        res.status(200).json({
+            success: true, data: result.rows
+        });
+
+    } catch (error) {
+        res.status(200).json({
+            success: false, message: error.message
         });
     }
 })
 
 app.put('/products/:id', async (req, res) => {
     const {id} = req.params;
-    const {title, image, description, price, categoryId} = req.body;
-    const response = await pool.query('update products  set title=$1, image=$2,description=$3,price=$4,categoryId=$5', [title, image, description, price, categoryId])
+    const {title, price, description, image, categoryId} = req.body;
+    const result = await pool.query('update products set title=$1, price=$2, description=$3, image=$4, categoryId=$5 where id=$6', [title, price, description, image, categoryId, id]);
+    if (!title || !price || !description || !image || !categoryId) {
+
+        if (!title) {
+            res.status(200).json({
+                success: false, message: 'title is required'
+            });
+        }
+        if (!price) {
+            res.status(200).json({
+                success: false, message: 'price is required'
+            });
+        }
+        if (!description) {
+            res.status(200).json({
+                success: false, message: 'description is required'
+            });
+        }
+        if (!image) {
+            res.status(200).json({
+                success: false, message: 'image is required'
+            });
+        }
+        if (!categoryId) {
+            res.status(200).json({
+                success: false, message: 'categoryId is required'
+            });
+        }
+
+        res.status(200).json({
+            success: false, message: 'All fields are required'
+        });
+    }
+    if (result.rows.length === 1) {
+        res.status(200).json({
+            success: true, data: result.rows
+        });
+    } else {
+        res.status(200).json({
+            success: false, message: 'Invalid id'
+        });
+    }
 });
 
 app.get('/products/:id', async (req, res) => {
@@ -63,16 +116,26 @@ app.get('/products/:id', async (req, res) => {
     const data = result.rows.length === 1;
     if (data) {
         res.status(200).json({
-            success: true,
-            data: result.rows
+            success: true, data: result.rows
         });
     } else {
         res.status(200).json({
-            success: false,
-            message: 'no data found'
+            success: false, message: 'no data found'
         });
     }
-})
+});
+
+
+//Category API s
+//[GET] /categories barcha categorylarni olish
+//[GET] /categories/:id
+// [POST] /categories/ create category
+// [PUT] / categories/:id update category
+// [DELETE]/categories/:id delete categories
+app.get('/categories', async (req, res) => {
+
+});
+
 
 app.listen(5000, () => {
     console.log('server is running ');
