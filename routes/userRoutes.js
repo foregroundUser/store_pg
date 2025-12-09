d1be0835358fc435d2cb5d0375867993b8824237
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-12-09 17:49:56 +0500

    refactor: simplify category and user retrieval by reusing common controller logic

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,7 @@ const express = require('express');
 const {getAllUsers, deleteUser, updateUser, addUser} = require("../controllers/userController");
 const userRouter = express.Router();
 userRouter.post('/', addUser)
-
 userRouter.get('/', getAllUsers)
-
 userRouter.delete('/:id', deleteUser)
 
 userRouter.put('/:id', updateUser);
