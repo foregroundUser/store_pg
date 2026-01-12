faa1d9560084500074c41cafe79d60b38834f0b9
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2026-01-12 12:55:23 +0500

    all is good nice try diddy

--- server2.js
+++ server2.js
@@ -3,22 +3,19 @@ const app = express();
 app.use(express.json());
 
 app.listen(5001, () => {
-    console.log("GIRGITTON 5000-portda ishlayapti...");
+    console.log("GIRGITTON 5001-portda ishlayapti...");
 });
 
 app.use((req, res, next) => {
     const age = req.query.age;
-    if (!age) {
-        return res.status(401).json({message: "Age is required"});
-    }
-    if (age < 20) {
-        return res.status(403).json({message: "You`re too young"});
-    }
+    if (!age) return res.status(401).json({message: "Where age ?"});
+    if (age < 20) return res.status(403).json({message: "You are looking like a kid"});
     next();
 })
 
 app.get("/quote", (req, res) => {
     res.json({
-        "author": "Anonymous", "quote": "Sometimes you'll never know the value of a moment, until it becomes a memory."
+        "author": "Anonymous", "quote": "Sometimes you'll never know the value of a moment, until it becomes a memory.",
+
     });
 });
