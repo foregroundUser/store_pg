5a70171334bc84bec6369649343489a104694dca
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2026-01-12 13:20:41 +0500

    all is good nice try diddy

--- server2.js
+++ server2.js
@@ -6,16 +6,16 @@ app.listen(5001, () => {
     console.log("GIRGITTON 5001-portda ishlayapti...");
 });
 
-app.use((req, res, next) => {
-    const age = req.query.age;
-    if (!age) return res.status(401).json({message: "Where age ?"});
-    if (age < 20) return res.status(403).json({message: "You are looking like a kid"});
-    next();
-})
 
+/*
 app.get("/quote", (req, res) => {
     res.json({
         "author": "Anonymous", "quote": "Sometimes you'll never know the value of a moment, until it becomes a memory.",
 
     });
+});
+*/
+
+app.get("/login", (req, res) => {
+    res.json({message: "You are logged in successfully"});
 });
